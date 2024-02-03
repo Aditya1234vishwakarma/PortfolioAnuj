aa06285abec7ea5e1c91956089654c7afdc47182
Aditya1234vishwakarma <adityapwstudent@gmail.com>
2024-02-03 13:31:50 +0530

    project completed

--- hover.js
+++ hover.js
@@ -18,18 +18,18 @@ document.getElementById("a5").addEventListener("mouseover", ()=>{
     document.getElementById("line5").style.backgroundColor = "#fff";
     document.getElementById("line5").style.width = "30px";
 })
-document.getElementById("a6").addEventListener("mouseover", ()=>{
-    document.getElementById("line6").style.backgroundColor = "#fff";
-    document.getElementById("line6").style.width = "30px";
-})
-document.getElementById("a7").addEventListener("mouseover", ()=>{
-    document.getElementById("line7").style.backgroundColor = "#fff";
-    document.getElementById("line7").style.width = "30px";
-})
-document.getElementById("a8").addEventListener("mouseover", ()=>{
-    document.getElementById("line8").style.backgroundColor = "#fff";
-    document.getElementById("line8").style.width = "30px";
-})
+// document.getElementById("a6").addEventListener("mouseover", ()=>{
+//     document.getElementById("line6").style.backgroundColor = "#fff";
+//     document.getElementById("line6").style.width = "30px";
+// })
+// document.getElementById("a7").addEventListener("mouseover", ()=>{
+//     document.getElementById("line7").style.backgroundColor = "#fff";
+//     document.getElementById("line7").style.width = "30px";
+// })
+// document.getElementById("a8").addEventListener("mouseover", ()=>{
+//     document.getElementById("line8").style.backgroundColor = "#fff";
+//     document.getElementById("line8").style.width = "30px";
+// })
 document.getElementById("a9").addEventListener("mouseover", ()=>{
     document.getElementById("line9").style.backgroundColor = "#fff";
     document.getElementById("line9").style.width = "30px";
@@ -50,52 +50,55 @@ document.getElementById("a13").addEventListener("mouseover", ()=>{
     document.getElementById("line13").style.backgroundColor = "#fff";
     document.getElementById("line13").style.width = "30px";
 })
-document.getElementById("a14").addEventListener("mouseover", ()=>{
-    document.getElementById("line14").style.backgroundColor = "#fff";
-    document.getElementById("line14").style.width = "100%";
-})
 document.getElementById("a1").addEventListener("mouseleave", ()=>{
     document.getElementById("line1").style.backgroundColor = "#000";
+    document.getElementById("line1").style.width = "0px"
 })
 document.getElementById("a2").addEventListener("mouseleave", ()=>{
     document.getElementById("line2").style.backgroundColor = "#000";
+    document.getElementById("line2").style.width = "0px"
 })
 document.getElementById("a3").addEventListener("mouseleave", ()=>{
     document.getElementById("line3").style.backgroundColor = "#000";
+    document.getElementById("line3").style.width = "0px"
 })
 document.getElementById("a4").addEventListener("mouseleave", ()=>{
     document.getElementById("line4").style.backgroundColor = "#000";
+    document.getElementById("line4").style.width = "0px"
 })
 document.getElementById("a5").addEventListener("mouseleave", ()=>{
     document.getElementById("line5").style.backgroundColor = "#000";
+    document.getElementById("line5").style.width = "0px"
 })
-document.getElementById("a6").addEventListener("mouseleave", ()=>{
-    document.getElementById("line6").style.backgroundColor = "#000";
-})
-document.getElementById("a7").addEventListener("mouseleave", ()=>{
-    document.getElementById("line7").style.backgroundColor = "#000";
-})
-document.getElementById("a8").addEventListener("mouseleave", ()=>{
-    document.getElementById("line8").style.backgroundColor = "#000";
-})
+// document.getElementById("a6").addEventListener("mouseleave", ()=>{
+//     document.getElementById("line6").style.backgroundColor = "#000";
+//     document.getElementById("line6").style.width = "0px"
+// })
+// document.getElementById("a7").addEventListener("mouseleave", ()=>{
+//     document.getElementById("line7").style.backgroundColor = "#000";
+//     document.getElementById("line7").style.width = "0px"
+// })
+// document.getElementById("a8").addEventListener("mouseleave", ()=>{
+//     document.getElementById("line8").style.backgroundColor = "#000";
+//     document.getElementById("line8").style.width = "0px"
+// })
 document.getElementById("a9").addEventListener("mouseleave", ()=>{
     document.getElementById("line9").style.backgroundColor = "#000";
+    document.getElementById("line9").style.width = "0px"
 })
 document.getElementById("a10").addEventListener("mouseleave", ()=>{
     document.getElementById("line10").style.backgroundColor = "#000";
+    document.getElementById("line10").style.width = "0px"
 })
 document.getElementById("a11").addEventListener("mouseleave", ()=>{
     document.getElementById("line11").style.backgroundColor = "#000";
+    document.getElementById("line11").style.width = "0px"
 })
 document.getElementById("a12").addEventListener("mouseleave", ()=>{
     document.getElementById("line12").style.backgroundColor = "#000";
+    document.getElementById("line12").style.width = "0px"
 })
 document.getElementById("a13").addEventListener("mouseleave", ()=>{
     document.getElementById("line13").style.backgroundColor = "#000";
-})
-
-document.getElementById("a14").addEventListener("mouseleave", ()=>{
-    document.getElementById("line14").style.backgroundColor = "#000";
-    document.getElementById("line14").style.width = "40px";
-    
+    document.getElementById("line13").style.width = "0px"
 })
--- toggle.js
+++ toggle.js
@@ -3,17 +3,15 @@ document.getElementById("toggle-nav").addEventListener("click", () => {
     let navlist = document.getElementById("res-nav");
     let projectlist = document.getElementById("project-list")
     let rotate = document.getElementById("arrow1")
-    if (toggle1.style.top != "120px", navlist.style.top != "60px",  rotate.style.transform != "rotate(270deg)", projectlist.style.top != "145px") {
+    if (toggle1.style.top != "120px", navlist.style.top != "60px",  rotate.style.transform != "rotate(90deg)") {
         navlist.style.top = "60px";
         toggle1.style.top = "120px";
-        rotate.style.transform = "rotate(270deg)";
-        projectlist.style.top = "145px";
+        rotate.style.transform = "rotate(90deg)";
     }
     else {
-        rotate.style.transform = "rotate(90deg)";
+        rotate.style.transform = "rotate(270deg)";
         toggle1.style.top = "60px";
         navlist.style.top = "1px";
-        projectlist.style.top = "85px";
     }
 
 })
