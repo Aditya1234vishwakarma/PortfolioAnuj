373300a4a16529b929b5da85a2a7bd205a4fdd52
Aditya1234vishwakarma <adityapwstudent@gmail.com>
2024-02-18 16:24:27 +0530

    initial commit

--- name.js
+++ name.js
@@ -1,138 +1,28 @@
-function name() {
-    setTimeout(() => {
-        document.getElementById("letter1").innerHTML = "A"
+const text = "Anuj Vishwakarma";
+const speed = 400; 
 
-    }, 200);
-    setTimeout(() => {
-        document.getElementById("letter2").innerHTML = "n"
+let index = 0;
+let direction = 1; 
 
-    }, 400);
-    setTimeout(() => {
-        document.getElementById("letter3").innerHTML = "u"
+function typeText() {
+    const container = document.getElementById("text-container");
+    const char = text.charAt(index);
 
-    }, 600);
-    setTimeout(() => {
-        document.getElementById("letter4").innerHTML = "j"
+    
+    if (index === text.length) {
+        direction = -1; 
+    } else if (index === 0) {
+        direction = 1; 
+    }
+    if (direction === 1) {
+        container.innerHTML += char;
+        index++;
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -1);
+        index--;
+    }
 
-    }, 800);
-    setTimeout(() => {
-        document.getElementById("letter5").innerHTML = " "
+    setTimeout(typeText, speed);
+}
 
-    }, 1000);
-    setTimeout(() => {
-        document.getElementById("letter6").innerHTML = "V"
-
-    }, 1200);
-    setTimeout(() => {
-        document.getElementById("letter7").innerHTML = "i"
-
-    }, 1400);
-    setTimeout(() => {
-        document.getElementById("letter8").innerHTML = "s"
-
-    }, 1600);
-    setTimeout(() => {
-        document.getElementById("letter9").innerHTML = "h"
-
-    }, 1800);
-    setTimeout(() => {
-        document.getElementById("letter10").innerHTML = "w"
-
-    }, 2000);
-    setTimeout(() => {
-        document.getElementById("letter11").innerHTML = "a"
-
-    }, 2200);
-    setTimeout(() => {
-        document.getElementById("letter12").innerHTML = "k"
-
-    }, 2400);
-    setTimeout(() => {
-        document.getElementById("letter13").innerHTML = "a"
-
-    }, 2600);
-    setTimeout(() => {
-        document.getElementById("letter14").innerHTML = "r"
-
-    }, 2800);
-    setTimeout(() => {
-        document.getElementById("letter15").innerHTML = "m"
-
-    }, 3000);
-    setTimeout(() => {
-        document.getElementById("letter16").innerHTML = "a"
-
-    }, 3200);
-    setTimeout(() => {
-        document.getElementById("letter1").innerHTML = ""
-
-    }, 7200);
-    setTimeout(() => {
-        document.getElementById("letter2").innerHTML = ""
-
-    }, 7000);
-    setTimeout(() => {
-        document.getElementById("letter3").innerHTML = ""
-
-    }, 6800);
-    setTimeout(() => {
-        document.getElementById("letter4").innerHTML = ""
-
-    }, 6600);
-    setTimeout(() => {
-        document.getElementById("letter5").innerHTML = ""
-
-    }, 6400);
-    setTimeout(() => {
-        document.getElementById("letter6").innerHTML = ""
-
-    }, 6200);
-    setTimeout(() => {
-        document.getElementById("letter7").innerHTML = ""
-
-    }, 6000);
-    setTimeout(() => {
-        document.getElementById("letter8").innerHTML = ""
-
-    }, 5800);
-    setTimeout(() => {
-        document.getElementById("letter9").innerHTML = ""
-
-    }, 5600);
-    setTimeout(() => {
-        document.getElementById("letter10").innerHTML = ""
-
-    }, 5400);
-    setTimeout(() => {
-        document.getElementById("letter11").innerHTML = ""
-
-    }, 5200);
-    setTimeout(() => {
-        document.getElementById("letter12").innerHTML = ""
-
-    }, 5000);
-    setTimeout(() => {
-        document.getElementById("letter13").innerHTML = ""
-
-    }, 4800);
-    setTimeout(() => {
-        document.getElementById("letter14").innerHTML = ""
-
-    }, 4600);
-    setTimeout(() => {
-        document.getElementById("letter15").innerHTML = ""
-
-    }, 4200);
-    setTimeout(() => {
-        document.getElementById("letter16").innerHTML = ""
-
-    }, 4000);
-};
-
-// setTimeout(() => {
-//     name()
-// }, 500);
-
-setInterval(() => {
-    name();
-}, 8000);
+setInterval(typeText(), 3000);
